b8e897a0cdfc03244282bc53783c8b006ee85f41
Shubhodeep Mukherjee <shubhodeep9@gmail.com>
2016-08-31 08:24:41 +0530

    Modular

---
+++ 5Modular/module.js
@@ -0,0 +1,20 @@
+var fs = require('fs');
+var path = require('path');
+
+module.exports = function(dirname, ext, callback) {
+  var extension = "." + ext;
+  fs.readdir(dirname, function(err, files) {
+    if (err) {
+      callback(err, null);
+    }
+    else {
+      result = [];
+      files.forEach(function(entry) {
+        if (path.extname(entry) == extension) {
+          result.push(entry);
+        }
+      });
+      callback(null, result);
+    }
+  });
+};
